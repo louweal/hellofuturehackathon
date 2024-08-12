37467706dfdf01cbdd4c08274767ed5d345e5d25
louweal <anneloeslouwe@live.nl>
2024-08-12 19:53:50 +0200

    create realviews transaction button

--- realviews/main.php
+++ realviews/main.php
@@ -21,6 +21,56 @@ function init_realviews_function()
     require_once plugin_dir_path(__FILE__) . 'lib/shortcodes.php';
 }
 
+// must be hooked from main ?!
+add_action('acf/init', 'realviews_block_init', 10);
+function realviews_block_init()
+{
+    // Check function exists.
+    if (function_exists('acf_register_block_type')) {
+        // Register the realviews transaction button block.
+        acf_register_block_type(array(
+            'name'              => 'realviews-transaction-button',
+            'title'             => __('Realviews Transaction Button', 'hfh'),
+            'description'       => __('Button for payments on Hedera with optional refund after review', 'hfh'),
+            'render_template'   => dirname(plugin_dir_path(__FILE__)) . '/realviews/blocks/realviews-transaction-button.php',
+            'mode'              => 'edit',
+            'category'          => 'common',
+            'icon'              => 'money-alt',
+            'keywords'          => array('hederapay', 'hedera', 'transaction', 'button', 'realviews', 'review'),
+        ));
+    }
+}
+
+add_action('acf/init', 'add_cashback_field_group', 12);
+function add_cashback_field_group()
+{
+    if (function_exists('acf_add_local_field_group')) {
+        acf_add_local_field_group(array(
+            'key' => 'group_realviews_cashback_field',
+            'title' => 'Cashback field',
+            'fields' => array(
+                array(
+                    'key' => 'field_cashback',
+                    'label' => 'Cashback after review',
+                    'name' => 'field_cashback',
+                    'type' => 'text',
+                    'required' => 0,
+                    'instructions' => 'Can be an amount in [currency] or a percentage (use %). Leave empty to disable.',
+                    'default_value' => '3%'
+                ),
+            ),
+            'location' => array(
+                array(
+                    array(
+                        'param' => 'block',
+                        'operator' => '==',
+                        'value' => 'acf/realviews-transaction-button',
+                    ),
+                ),
+            ),
+        ));
+    }
+}
 
 
 // must be hooked from main ?!
@@ -82,10 +132,6 @@ function add_latest_reviews_field_groups()
         ));
     }
 }
-
-
-// todo add realviews checkbox?
-
 
 
 add_action('plugins_loaded', function () {
