92189884f48bab5492b650baaa5fb7fdf807448f
louweal <anneloeslouwe@live.nl>
2024-08-12 16:02:14 +0200

    implementing HCS

--- realviews/src/js/modules/initConsensus.js
+++ realviews/src/js/modules/initConsensus.js
@@ -1,30 +1,71 @@
-const {
-    AccountId,
-    PrivateKey,
+import { HashConnect, HashConnectConnectionState } from 'hashconnect';
+import {
     Client,
+    AccountId,
+    PublicKey,
     TopicCreateTransaction,
     TopicMessageQuery,
     TopicMessageSubmitTransaction,
-} = require('@hashgraph/sdk');
-require('dotenv').config();
+    PrivateKey,
+} from '@hashgraph/sdk';
+import { LedgerId } from '@hashgraph/sdk';
+
+export const greet = () => {
+    console.log('Greeting!');
+};
 
 export const initConsensus = () => {
-    async function createPrivateTopic() {
-        const myAccountId = process.env.MY_ACCOUNT_ID;
-        const myPrivateKey = process.env.MY_PRIVATE_KEY;
+    let hashconnect;
+    let state = HashConnectConnectionState.Disconnected;
+    let pairingData;
 
-        if (!myAccountId || !myPrivateKey) {
-            throw new Error('Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present');
-        }
+    let metadata = document.querySelector('#hederapay-app-metadata');
 
-        const client = Client.forTestnet();
-        client.setOperator(myAccountId, myPrivateKey);
+    let appMetadata = {};
+    if (metadata) {
+        appMetadata = {
+            name: metadata.dataset.name,
+            description: metadata.dataset.description,
+            icons: [metadata.dataset.icon],
+            url: metadata.dataset.url,
+        };
+    }
 
-        // Create a new topic
-        let txResponse = await new TopicCreateTransaction().setSubmitKey(myPrivateKey.publicKey).execute(client);
+    let topicButton = document.querySelector('.realviews-create-topic-button');
+
+    if (topicButton) {
+        topicButton.addEventListener('click', async function () {
+            console.log('clicked');
+
+            let network = topicButton.dataset.network;
+            if (!pairingData) {
+                await init(network); //connect
+            } else {
+                hashconnect.disconnect(); // disconnect wallet
+            }
+
+            let topicId = await createPrivateTopic(pairingData);
+            console.log(topicId);
+        });
+    }
+
+    async function createPrivateTopic(pairingData) {
+        let fromAccount = AccountId.fromString(pairingData.accountIds[0]); // assumes paired and takes first paired account id
+
+        let signer = hashconnect.getSigner(fromAccount);
+        // let test = PrivateKey.fromString(pairingData.accountIds[0]);
+        const submitKey = PrivateKey.generateED25519();
+
+        let transaction = await new TopicCreateTransaction()
+            .setTopicMemo('topic memo')
+            .setTransactionMemo('transaction memo')
+            .setSubmitKey(submitKey)
+            .freezeWithSigner(signer);
+
+        let response = await transaction.executeWithSigner(signer);
 
         // Grab the newly generated topic ID
-        let receipt = await txResponse.getReceipt(client);
+        let receipt = await response.getReceiptWithSigner(signer);
         let topicId = receipt.topicId;
         console.log(`Your topic ID is: ${topicId}`);
 
@@ -55,5 +96,55 @@ export const initConsensus = () => {
         // Get the status of the transaction
         const transactionStatus = getReceipt.status;
         console.log('The message transaction status: ' + transactionStatus.toString());
+    }
+
+    function getNetworkId(network) {
+        let networkId;
+        switch (network) {
+            case 'testnet':
+                networkId = LedgerId.TESTNET;
+                break;
+            case 'previewnet':
+                networkId = LedgerId.PREVIEWNET;
+                break;
+            case 'mainnet':
+                networkId = LedgerId.MAINNET;
+                break;
+            default:
+                networkId = LedgerId.TESTNET;
+        }
+        return networkId;
+    }
+
+    async function init(network) {
+        let networkId = getNetworkId(network);
+        // Create the hashconnect instance
+        hashconnect = new HashConnect(networkId, '606201a2da45f68c8084e2eea1f14ad7', appMetadata, true);
+
+        // Register events
+        setUpHashConnectEvents();
+
+        // Initialize
+        await hashconnect.init();
+
+        // Open pairing modal
+        if (!pairingData) {
+            hashconnect.openPairingModal();
+        }
+    }
+
+    function setUpHashConnectEvents() {
+        hashconnect.pairingEvent.on((newPairing) => {
+            pairingData = newPairing;
+            // let id = pairingData.accountIds[0];
+        });
+
+        hashconnect.disconnectionEvent.on(() => {
+            pairingData = null;
+        });
+
+        hashconnect.connectionStatusChangeEvent.on((connectionStatus) => {
+            state = connectionStatus;
+        });
     }
 };
