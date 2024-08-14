1f55dc676b3ff32460f482da2a33ac6d9e1e848c
louweal <anneloeslouwe@live.nl>
2024-08-14 15:55:13 +0200

    conditionally show hero

--- template-parts/hero/hero-home.php
+++ template-parts/hero/hero-home.php
@@ -14,37 +14,41 @@ $image_5 = get_field('hero_image_5');
 $image_6 = get_field('hero_image_6');
 $images = [$image_1, $image_2, $image_3, $image_4, $image_5, $image_6];
 
+if ($title) {
+
 ?>
 
-<header class="hero-home">
-    <div class="hero-home__bg"></div>
-    <div class="container hero-home__content order-2 sm:order-1">
-        <div class="row">
-            <div class="box sm:box-8 lg:box-6">
-                <?php if ($title) { ?>
-                    <div class="editor">
-                        <?php echo $title; ?>
-                    </div> <?php }; //if 
-                            ?>
-                <?php if ($link) { ?>
-                    <?php the_link($link, 'btn'); ?>
-                <?php } ?>
+    <header class="hero-home">
+        <div class="hero-home__bg"></div>
+        <div class="container hero-home__content order-2 sm:order-1">
+            <div class="row">
+                <div class="box sm:box-8 lg:box-6">
+                    <?php if ($title) { ?>
+                        <div class="editor">
+                            <?php echo $title; ?>
+                        </div> <?php }; //if 
+                                ?>
+                    <?php if ($link) { ?>
+                        <?php the_link($link, 'btn'); ?>
+                    <?php } ?>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="hero-home__slider order-1 sm:order-2">
-        <div class="slider slider--home swiper js-slider" data-slider="home">
-            <div class="slider__wrapper swiper-wrapper">
-                <?php foreach ($images as $image) { ?>
-                    <div class="slider__slide swiper-slide">
-                        <picture>
-                            <img src="<?php echo $image['sizes']['medium_large']; ?>" alt="<?php echo $image['alt']; ?>">
-                        </picture>
-                    </div>
-                <?php }; //foreach 
-                ?>
+        <div class="hero-home__slider order-1 sm:order-2">
+            <div class="slider slider--home swiper js-slider" data-slider="home">
+                <div class="slider__wrapper swiper-wrapper">
+                    <?php foreach ($images as $image) { ?>
+                        <div class="slider__slide swiper-slide">
+                            <picture>
+                                <img src="<?php echo $image['sizes']['medium_large']; ?>" alt="<?php echo $image['alt']; ?>">
+                            </picture>
+                        </div>
+                    <?php }; //foreach 
+                    ?>
+                </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
+
+<? } ?>
