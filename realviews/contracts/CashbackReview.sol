82f0aba10618859204f4272d4308202e5134598d
louweal <anneloeslouwe@live.nl>
2024-08-15 19:19:12 +0200

    temporary CashbackReview contracts

--- realviews/contracts/CashbackReview.sol
+++ realviews/contracts/CashbackReview.sol
@@ -1,33 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-contract ReviewContract {
+contract CashbackReview {
     enum State { Pending, Reviewed }
     
     // State variables
     // transactionId = customer@bSeconds.bNanoseconds
+    // Slot 1
     address payable public customer; 
+    // Slot 2
     address payable public shopOwner;
-    uint256 public bSeconds; // buy timestamp in seconds
-    uint256 public bNanoseconds; // buy timestamp nanoseconds
+    // Slot 3
     uint64 public cashback;
+    uint32 public bSeconds; // buy timestamp in seconds
+    uint32 public bNanoseconds; // buy timestamp nanoseconds
     uint32 public rSeconds; // review timestamp in seconds, range valid until 7 February 2106 06:28:15
     uint32 public iSeconds; // minimum number of seconds (interval) between buy and review
-    string public cid;
     State public state;
+    // Slot 4
+    string public cid;
 
     // Payable constructor
-    constructor(uint32 _bSeconds, uint256 _bNanoseconds, uint64 _amount, uint64 _cashback, address _shopOwner, uint32 _iSeconds ) payable {
-        require(msg.value == _amount, "Incorrect value send"); 
-        customer = payable(msg.sender);
+    constructor(address _customer, uint32 _bSeconds, uint32 _bNanoseconds, uint64 _cashback, address _shopOwner, uint32 _iSeconds ) payable {
+        // require(msg.value > 0, "No value send"); 
+        // require(msg.value >= cashback, "Incorrect value send"); 
+        customer = payable(_customer);
         shopOwner = payable(_shopOwner);
         bSeconds = _bSeconds;
         bNanoseconds = _bNanoseconds;
         cashback = _cashback;
         iSeconds = _iSeconds;
         state = State.Pending; 
-        shopOwner.transfer(_amount - cashback);
+        // require(shopOwner.send(msg.value - cashback), "Failed to send");
     }
+
 
     // Payable function to attach a review
     function AttachReview(string memory _cid) external inState(State.Pending) onlyCustomer {
